f6e492adb5a9b7e3423451653a5f57638fa8eeb5
Sussybakabeep30 <vanshikavishal354@gmail.com>
2025-04-21 21:44:42 +0530

    changed browserouter to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import Loading from './components/Loading';
 
@@ -8,22 +8,20 @@ const Portfolio  = lazy(() => import('./pages/Portfolio'));
 const ClientForm = lazy(() => import('./pages/ClientForm'));
 const MyRequests = lazy(() => import('./pages/Myrequests'));
 
-
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
+      <HashRouter> 
         <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/client-form" element={<ClientForm />} />
-            <Route path="/MyRequests" element={<MyRequests />} />
+            <Route path="/myrequests" element={<MyRequests />} />
             <Route path="*" element={<Navigate to="/" />} />
-            
           </Routes>
         </Suspense>
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   );
 }
